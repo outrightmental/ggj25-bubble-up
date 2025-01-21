ddfd195ceece04abccbdb1af021213a172976237
Charney Kaye <charneykaye@gmail.com>
2025-01-21 08:09:11 -0800

    Stable experiment 2 and 3

--- project/scenes/main.gd
+++ project/scenes/main.gd
@@ -8,6 +8,10 @@ func _on_navigate_experiment_2() -> void:
 	_on_navigate_experiment(2)
 
 
+func _on_navigate_experiment_3() -> void:
+	_on_navigate_experiment(3)
+
+
 func _on_navigate_experiment(experiment_number: int) -> void:
 	get_tree().change_scene_to_file("res://scenes/experiment_" + str(experiment_number) + "/experiment_" + str(experiment_number) + ".tscn")
 	
