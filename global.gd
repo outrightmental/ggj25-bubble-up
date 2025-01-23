4492289d7003ea6d8bb9066a28d01fdb6238af1a
Nick Charney Kaye <charneykaye@users.noreply.github.com>
2025-01-23 00:43:51 -0800

    Bubbles merge on collision (#22)
    When bubbles collide with each other, they merge to create larger bubble. Bubbles merge into a larger bubble when two of them hit each other with enough force (although they can still rest gently next to each other)
    
    Implements #17

--- global.gd
+++ global.gd
@@ -5,7 +5,8 @@ var drag_behavior: DragBehavior = DragBehavior.APPLY_DAMPENED_FORCE
 var drag_factor: float          = 0.1
 var dragging_force_min: float   = 5 # How much force is applied when dragging
 var dragging_force_max: float   = 200 # How much force is applied when dragging
-
+var bubble_collision_merge_accel_threshold = 5000
+var bubble_collision_cooldown_millis = 100
 
 func set_drag_behavior(value: DragBehavior):
 	drag_behavior = value
