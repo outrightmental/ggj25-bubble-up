09689241d1c614361361d0bfd66d8a62f6b6aa19
Nick Charney Kaye <charneykaye@gmail.com>
2025-01-22 16:45:55 -0800

    Bubble emitter
    Game starts with some amount of bubbles being blown at the bottom of the ocean (ala whale exhaling)
    
    Implements #17

---
+++ models/bubble/bubble_emitter.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var spawn_rate: float = 1.0  # Time between bubble spawns
+@export var max_bubbles: int = 10    # Maximum number of bubbles to spawn
+@export var spread_x: int = 100      # Maximum horizontal distance from the emitter
+@export var spread_y: int = 50       # Maximum vertical distance from the emitter
+var current_bubbles: int = 0
+
+var timer: Timer
+
+func _ready():
+	timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 1 / spawn_rate
+	timer.connect("timeout", Callable(self, "_on_Timer_timeout"))
+	timer.start()
+
+func _on_Timer_timeout():
+	if current_bubbles < max_bubbles:
+		_spawn_bubble()
+		current_bubbles += 1
+
+# Spawn at the emitter's position +/- random spread in x and y
+func _spawn_bubble():
+	var new_bubble = preload("res://models/bubble/bubble.tscn").instantiate()
+	var spread = Vector2(randf_range(-spread_x, spread_x), randf_range(-spread_y, spread_y))
+	new_bubble.variant = randi() % 4
+	new_bubble.position = spread
+	self.add_child(new_bubble)
