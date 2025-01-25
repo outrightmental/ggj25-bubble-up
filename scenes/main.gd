beab2826ec505caa90779c7c0430f5b55510d8a0
Nick Charney Kaye <charneykaye@gmail.com>
2025-01-24 20:03:30 -0800

    Checkkpoint

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
-# Called when the scene is added to the tree
-func _ready():
-	get_tree().change_scene_to_file("res://scenes/experiments/experiments.tscn")
+func _on_start_game() -> void:
+	get_tree().change_scene("res://scenes/play/play.tscn")
