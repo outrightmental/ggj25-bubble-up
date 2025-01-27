f37cce6bdc8c9aa74e751a0b893e867697131d5c
Nick Charney Kaye <charneykaye@users.noreply.github.com>
2025-01-26 16:14:37 -0800

    Game Menu (#53)
    Closes #47

--- models/hud/hud.gd
+++ models/hud/hud.gd
@@ -3,13 +3,20 @@ extends Control
 @onready var bar_total: ColorRect = $"MarginContainer/VBoxContainer/HBoxContainer/Bar/Total"
 @onready var bar_score: ColorRect = $"MarginContainer/VBoxContainer/HBoxContainer/Bar/Score"
 @onready var bar_wasted: ColorRect = $"MarginContainer/VBoxContainer/HBoxContainer/Bar/Wasted"
+@onready var menu_container: ColorRect = $"MenuContainer"
+@onready var menu_button: Button = $"MarginContainer/VBoxContainer/HBoxContainer/Buttons/MenuButton"
+@onready var menu_resume_button: Button = $"MenuContainer/MarginContainer/VBoxContainer/ResumeButton"
+@onready var menu_main_button: Button = $"MenuContainer/MarginContainer/VBoxContainer/MainButton"
+@onready var menu_restart_button: Button = $"MenuContainer/MarginContainer/VBoxContainer/RestartButton"
 
 var total: float = 0
 
 func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	SignalBus.update_wasted.connect(_update_wasted)
 	SignalBus.update_score.connect(_update_score)
 	SignalBus.update_total.connect(_update_total)
+	menu_container.hide()	
 
 
 func _update_total(mass: float) -> void:
@@ -33,9 +40,35 @@ func _update_score(value: int) -> void:
 		bar_score.scale.x = 0
 
 
-func _on_navigate_main() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+func _on_menu_open() -> void:
+	menu_container.show()
+	menu_button.hide()
+	_pause()
 
 
-func _on_navigate_reset() -> void:
+func _on_menu_close() -> void:
+	menu_container.hide()
+	menu_button.show()
+	_resume()
+
+func _on_menu_select_resume() -> void:
+	_on_menu_close()
 	get_tree().reload_current_scene()
+
+
+func _on_menu_select_main() -> void:
+	_on_menu_close()
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	
+	
+func _on_menu_select_restart() -> void:
+	_on_menu_close()
+	get_tree().reload_current_scene()
+
+
+func _pause() -> void:
+	get_tree().set_deferred("paused", true)
+	
+	
+func _resume() -> void:
+	get_tree().set_deferred("paused", false)
